a3e039656f6586c0716a480bbc5a1e36ce65e45b
CHOI <byung803@gmail.com>
2017-12-20 16:43:12 +0900

    lecture finished

---
+++ app/main_window.js
@@ -0,0 +1,22 @@
+const electron = require('electron');
+const { BrowserWindow } = electron;
+
+class mainWindow extends BrowserWindow {
+  constructor() {
+    super({
+      height: 500,
+      width: 300,
+      frame: false,
+      resizable: false,
+      show: false,
+      webPreferences: { backgroundThrottling: false }
+    });
+    this.on('blur', this.onBlur.bind(this));
+  }
+
+  onBlur() {
+      this.hide();
+  }
+}
+
+module.exports = mainWindow;
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const path = require('path');
 const electron = require('electron');
+const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
 
-const { app, BrowserWindow, Tray } = electron;
+const { app, ipcMain } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    height: 500,
-    width: 300,
-    frame: false,
-    resizable: false
-  });
+  mainWindow = new MainWindow();
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+  process.platform === 'win32' ? mainWindow.setSkipTaskbar(true) : app.dock.hide();
 
-  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';  
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  new Tray(iconPath);
+  tray = new TimerTray(iconPath, mainWindow);
+});
+
+ipcMain.on('update-timer', (event, timeLeft) => {
+  tray.setTitle(timeLeft);
 });
